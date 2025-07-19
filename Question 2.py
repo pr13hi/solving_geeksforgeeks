6c41f296a1cf474de0030e05eba489b732192e80
Prachi <prachishaw04@gmail.com>
2025-07-19 23:29:49 +0530

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,11 @@
+# Question : Mark Even and Odd
+#           Given a positive integer x. Your task is to check, if it is even or odd (Any number that gives zero as remainder when divided by 2 is an even number)
+
+# Solution :
+
+def checkOddEven(x):
+    if (x % 2 == 0):
+        return True
+    else:
+        return False
+
