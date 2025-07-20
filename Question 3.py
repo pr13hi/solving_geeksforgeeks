94df7c10c5f2af870ca1fad3248bca63c464ad9b
Prachi <prachishaw04@gmail.com>
2025-07-20 21:44:05 +0530

    Update Question 3.py

--- Question 3.py
+++ Question 3.py
@@ -4,9 +4,8 @@
 
 # Solution :
 
-class Solution:
-    def checkStatus(self, a, b, flag):
-        if flag:
-            return a < 0 and b < 0
-        else:
-            return (a < 0 <= b) or (b < 0 <= a)
+def friends_in_trouble(j_angry, s_angry):
+    if (j_angry==True and s_angry==True) or (j_angry==False and s_angry==False):
+        return True
+    else:
+        return False
