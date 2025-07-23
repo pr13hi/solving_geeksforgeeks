d8ce7b1efb04f4bad91ecad1d4bf7d8fe1d748f3
Prachi <prachishaw04@gmail.com>
2025-07-23 20:08:46 +0530

    Create Question 15.py

---
+++ Question 15.py
@@ -0,0 +1,10 @@
+# Question : Reverse an Array
+#            You are given an array of integers arr[]. Your task is to reverse the given array.
+#            Note: Modify the array in place.
+
+# Solution :
+
+class Solution:
+    def reverseArray(self, arr):
+        arr.reverse()
+        return arr
