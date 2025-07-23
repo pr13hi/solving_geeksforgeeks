f241140db2c84f5069acdeaa4ea02f8c3195c741
Prachi <prachishaw04@gmail.com>
2025-07-23 16:58:15 +0530

    Create Question 12.py

---
+++ Question 12.py
@@ -0,0 +1,7 @@
+# Question : Welcome aboard
+#            Given name of a person, the task is to welcome the person by printing the name with "Welcome". If name is "John", you should print "Welcome John".
+
+# Solution :
+
+def welcomeAboard(name):
+    print("Welcome",name) 
