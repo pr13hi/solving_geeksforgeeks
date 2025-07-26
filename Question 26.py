39b2cb975799aed659f05fdcefeb6efd58f76d39
Prachi <prachishaw04@gmail.com>
2025-07-26 22:57:04 +0530

    Create Question 26.py

---
+++ Question 26.py
@@ -0,0 +1,19 @@
+# Question : Implement Set in Python
+
+#            Given Q queries to do some operation on Set, the task is to perform all the queries in the Set as given below:
+#                   a. i element: Adds element to the set.
+#                   b. r element: Remove element from set.
+#                   c. s: Find sum of elements in set. Returns the sum of elements in Set.
+
+# Solution :
+
+def insert(s, element):
+    return s.add(element)
+
+def remove_from_set(s, element):
+    return s.discard(element)
+
+def sum_set(s):
+    return sum(s)
+    
+    
