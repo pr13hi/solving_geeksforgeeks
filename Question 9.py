539713cd4096680d82aa4e3b5054920749e850fb
Prachi <prachishaw04@gmail.com>
2025-07-27 22:36:48 +0530

    Update Question 9.py

--- Question 9.py
+++ Question 9.py
@@ -1,3 +1,5 @@
+# Concept name : Array
+
 # Question : Second Largest
 
 #            Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
