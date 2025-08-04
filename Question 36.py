56267f440e64544ef28e749d17c503f171f5cf68
Prachi <prachishaw04@gmail.com>
2025-08-04 23:25:49 +0530

    Update Question 36.py

--- Question 36.py
+++ Question 36.py
@@ -1,6 +1,6 @@
 # Concept name : Number Problem
 
-   # Question : Odd or Even
+   # Question : Prime Number
 
    #            Given a number n, determine whether it is a prime number or not.
    #            Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
